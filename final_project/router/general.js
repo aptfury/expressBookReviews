b7b750412e4a8cc9f191a149bf84e6b7e8aa600b
aptfury <blake.lemarr@outlook.com>
2024-12-30 21:32:44 -0500

    Added review search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -72,7 +72,15 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let review = books[isbn]["reviews"];
+  let title = books[isbn]["title"];
+
+  if (!isbn) {
+    res.send(`You must enter an ISBN to look up a review.`);
+  } else {
+    res.send(`Here\'s what people have to say about ${title}\n\n${review}`);
+  }
 });
 
 module.exports.general = public_users;
