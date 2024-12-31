553708d34d26bf0b974b417716aa8d5f89354e35
aptfury <blake.lemarr@outlook.com>
2024-12-30 21:52:25 -0500

    rough register setup

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,23 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({
+        "username": username,
+        "password": password
+      });
+
+      return res.status(200).json({ message: "User successfully registered. You may now log in." });
+    } else {
+      return res.status(404).json({ message: "This user already exists!" });
+    }
+  }
+  console.log(`user: ${username}\n\npass: ${password}`);
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
