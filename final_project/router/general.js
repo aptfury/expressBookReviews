20afb2303367df7aafc63fe5453ef3d4191c54aa
aptfury <blake.lemarr@outlook.com>
2025-01-01 04:05:44 -0500

    uninstalled axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,7 +2,6 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
-let axios = require('axios').defaults;
 
 const public_users = express.Router();
 
